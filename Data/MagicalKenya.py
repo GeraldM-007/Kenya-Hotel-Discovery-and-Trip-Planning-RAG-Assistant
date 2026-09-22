d8ed82a12d96cfd667faeb0095d7f63ce3a0b725
GeraldM-007 <geraldmuchuku237@gmail.com>
2026-09-22 22:31:47 +0300

    different approach

--- Data/MagicalKenya.py
+++ Data/MagicalKenya.py
@@ -1,40 +1,42 @@
 import requests 
 from bs4 import BeautifulSoup
-from app.config import MAGICAL_KENYA_URL, headers
+from app.config import DESTINATIONS, HEADERS
 
-url = MAGICAL_KENYA_URL
+destinations = DESTINATIONS
 
-def extract_data(url):
-    
-    response = requests.get(url, headers = headers, timeout = 30)
-    
-    response.raise_for_status()
-    
-    #parse the raw html into python objects
-    soup = BeautifulSoup(response.text, "html.parser")
+headers = HEADERS
 
-    #Remove dynamic/unwanted elements
-    for tag in soup(["script", "style", "noscript", "svg", "footer"]):
-        tag.decompose()
+def extract_data(destinations):
     
-    #Title
-    title = soup.find("h1")
-    
-    #paragraphs
-    paragraphs = [p.get_text(" ", strip = True) for p in soup.find_all("p") if p.get_text(strip=True)]
- 
-    #Headings
-    headings = [h.get_text(" ", strip=True) for h in soup.find_all(["h1","h2","h3","h4"]) if h.get_text(strip=True)]
-    
-    return {
-        "url": url,
-        "title": title.get_text(" ", strip = True) if title else None,
-        "paragraphs": paragraphs,
-        "headings": headings
-    }
-    
-data = extract_data(url)
+    for destination in destinations:
+        
+        url = destination["url"]
+        
+        response = requests.get(url, headers = headers, timeout = 30)
+        
+        response.raise_for_status()
+        
+        #parse the html into python objects
+        soup = BeautifulSoup(response.text, "html.parser")
+        
+        #remove dynamic/unwanted elements
+        for tag in soup(["script", "style", "noscript", "svg", "footer"]):
+            
+            tag.decompose()
+            
+        #Get the title of the page
+        title = [t.get_text(" ", strip = True) for t in soup.find_all("h1") if t.get_text(strip = True)]
+        print(title)
 
-print(data["title"])
-print(data["headings"])
-print(data["paragraphs"])
+        #Get paragraphs in the page
+        paragraphs = [p.get_text(" ", strip = True) for p in soup.find_all("p") if p.get_text(strip = True)]
+        print(paragraphs)
+        
+        #Get all the headings in the page
+        headings = [h.get_text(" ", strip = True) for h in soup.find_all(["h1","h2","h3","h4"]) if h.get_text(strip = True)]
+        print(headings)
+        
+    return response.raise_for_status()
+    
+data = extract_data(destinations)
+print("Raise for response was: ", data)
